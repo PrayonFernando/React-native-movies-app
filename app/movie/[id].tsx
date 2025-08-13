1dbcaae75f9886f4fa2f89c8369219ed93236a0f
KishorithaS <kishoritha4@gmail.com>
2025-08-13 17:30:01 +0530

    setting up

---
+++ app/movie/[id].tsx
@@ -0,0 +1,17 @@
+import { useLocalSearchParams } from "expo-router";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const details = () => {
+  const { id } = useLocalSearchParams();
+
+  return (
+    <View>
+      <Text>Movie Details: {id}</Text>
+    </View>
+  );
+};
+
+export default details;
+
+const styles = StyleSheet.create({});
