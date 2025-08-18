7a0918bc9b4ab0472be9eeaf18fd3a917016a554
KishorithaS <kishoritha4@gmail.com>
2025-08-18 10:59:08 +0530

    database connection

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -12,11 +12,11 @@ const MovieCard = ({
 }: Movie) => {
   return (
     <Link href={`/movies/${id}`} asChild>
-      <TouchableOpacity className="w-[30px]">
+      <TouchableOpacity className="w-[30%]">
         <Image
           source={{
             uri: poster_path
-              ? `https://imagee.tmdb.org/t/p/w500${poster_path}`
+              ? `https://image.tmdb.org/t/p/w500${poster_path}`
               : "https://placehold.co/600x400/1a1a1a/ffffff.png",
           }}
           className="w-full h-52 rounded-lg"
